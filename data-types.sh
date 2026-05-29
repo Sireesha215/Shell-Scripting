17c55201d2bcef1dc2ccf5bfef38efe1730a70b0
Sireesha <sireesha.lakkanaboina@gmail.com>
2026-05-29 15:03:35 +0530

    data types

--- data-types.sh
+++ data-types.sh
@@ -13,4 +13,4 @@ echo " Fruits are : ${FRUITS[@]}"
 echo "First fruit is : ${FRUITS[0]}"
 echo "Secon Fruit is : ${FRUITS[1]}"
 
-echo "Third Fruit is : $(FRUITS[2])"
+echo "Third Fruit is : ${FRUITS[2]}"

1fdd2a3eb3e5808919bf9ae75c3b89cc2a382761
Sireesha <sireesha.lakkanaboina@gmail.com>
2026-05-30 14:54:34 +0530

    conditions Scipt

---
+++ logfile.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/logs/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the Script with root user access"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+      echo "$2.....FAILURE"
+      exit 1
+    else
+      echo " $2....SUCCESS"
+    fi
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+
+VALIDATE $? "Nginx Installation"
+
+dnf install mysql -y &>> $LOGS_FILE
+
+VALIDATE $? "Mysql Installation"
+
+dnf install nodejs -y &>> $LOGS_FILE
+
+VALIDATE $? "nodejs Installation"
+
+

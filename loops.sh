2264b8762f311e489fe98e75053eac5b6452a00a
Sireesha <sireesha.lakkanaboina@gmail.com>
2026-05-30 15:45:32 +0530

    conditions Scipt

--- loops.sh
+++ loops.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-for i in {1..100}
-do 
-   echo $i
+USERID=$(id -u)
+LOGS_FOLDER="/var/logs/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the Script with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+      echo "$2.....FAILURE" | tee -a $LOGS_FILE
+      exit 1
+    else
+      echo " $2....SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@
+do
+  dnf install $package -y
+  VALIDATE $? "$package installtion"
 done

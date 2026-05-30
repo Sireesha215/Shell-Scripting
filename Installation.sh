0b4cdc1829d59bc294b8b9cd35d752a38faf1c10
Sireesha <sireesha.lakkanaboina@gmail.com>
2026-05-30 12:36:11 +0530

    conditions Scipt

---
+++ Installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the Script with root user access"
+    exit 1
+fi
+
+echo "Installing nginx........"
+dnf install nginx -y

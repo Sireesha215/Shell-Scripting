2f9d3fe7e8185fba841c53d51cbb8d0d243b49f0
Sireesha <sireesha.lakkanaboina@gmail.com>
2026-05-30 12:26:26 +0530

    conditions Scipt

--- conditions.sh
+++ conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-NUM=100
+NUM=$1
 
-if [ $NUM -gt 10 ]; then
-   echo "Given number is :"
+if [ $NUM -gt 0 ]; then
+   echo "Given number is Positive number"
 else
    echo "Given number is negitive number"
 fi
